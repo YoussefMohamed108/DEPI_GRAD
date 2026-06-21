a5049031354feec4747d291d53203f1912cfeabb
YoussefMohamed108 <168129774+YoussefMohamed108@users.noreply.github.com>
2026-06-22 02:27:02 +0300

    Update predict.py

--- api/predict.py
+++ api/predict.py
@@ -3,15 +3,16 @@ import pickle
 import pandas as pd
 from pathlib import Path
 
-app = Flask(**name**)
+app = Flask(__name__)
 
-BASE_DIR = Path(**file**).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 with open(BASE_DIR / "best_model_xgboost.pkl", "rb") as f:
-model = pickle.load(f)
+    model = pickle.load(f)
 
 with open(BASE_DIR / "best_threshold.pkl", "rb") as f:
-threshold = pickle.load(f)
+    threshold = pickle.load(f)
+
 
 FEATURE_COLUMNS = [
 "Year",
@@ -35,8 +36,10 @@ FEATURE_COLUMNS = [
 
 @app.route("/api/predict", methods=["POST"])
 def predict():
-try:
-data = request.get_json()
+    try:
+        data = request.get_json()
+
+
 
 ```
     df = pd.DataFrame([data])
