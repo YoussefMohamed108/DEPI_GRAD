2dc55ee82858650ca2d94c6553e50ce5d10c437e
YoussefMohamed108 <168129774+YoussefMohamed108@users.noreply.github.com>
2026-06-22 02:08:34 +0300

    Add files via upload

---
+++ api/predict.py
@@ -0,0 +1,103 @@
+from http.server import BaseHTTPRequestHandler
+import json
+import pickle
+import pandas as pd
+from pathlib import Path
+
+BASE_DIR = Path(**file**).resolve().parent.parent
+
+with open(BASE_DIR / "best_model_xgboost.pkl", "rb") as f:
+model = pickle.load(f)
+
+with open(BASE_DIR / "best_threshold.pkl", "rb") as f:
+threshold = pickle.load(f)
+
+FEATURE_COLUMNS = [
+"Year",
+"Quarter",
+"Month",
+"DayofMonth",
+"DayOfWeek",
+"WeekOfYear",
+"CRSDepTime",
+"CRSArrTime",
+"CRSElapsedTime",
+"Distance",
+"Cancelled",
+"Diverted",
+"DivAirportLandings",
+"Origin",
+"Dest",
+"Airline",
+"Operating_Airline"
+]
+
+class handler(BaseHTTPRequestHandler):
+
+```
+def do_POST(self):
+
+    try:
+        content_length = int(self.headers["Content-Length"])
+        body = self.rfile.read(content_length)
+
+        data = json.loads(body)
+
+        df = pd.DataFrame([data])
+
+        for col in FEATURE_COLUMNS:
+            if col not in df.columns:
+                df[col] = None
+
+        df = df[FEATURE_COLUMNS]
+
+        probability = float(model.predict_proba(df)[0][1])
+
+        delayed = probability >= threshold
+
+        confidence_score = abs(probability - threshold)
+
+        if confidence_score >= 0.30:
+            confidence = "High"
+        elif confidence_score >= 0.15:
+            confidence = "Medium"
+        else:
+            confidence = "Low"
+
+        response = {
+            "delayed": delayed,
+            "probability": round(probability * 100, 2),
+            "confidence": confidence,
+            "threshold": round(threshold * 100, 2)
+        }
+
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.end_headers()
+
+        self.wfile.write(json.dumps(response).encode())
+
+    except Exception as e:
+
+        self.send_response(500)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+
+        self.wfile.write(
+            json.dumps({"error": str(e)}).encode()
+        )
+
+def do_OPTIONS(self):
+    self.send_response(200)
+    self.send_header("Access-Control-Allow-Origin", "*")
+    self.send_header(
+        "Access-Control-Allow-Headers",
+        "Content-Type"
+    )
+    self.send_header(
+        "Access-Control-Allow-Methods",
+        "POST, OPTIONS"
+    )
+    self.end_headers()
+```
